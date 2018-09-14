028e2e086be25f84e228de13e301377d0ce4b750
Reece_Collett <rcollett@spartaglobal.com>
2018-09-14 11:51:18 +0100

    Changed name from app/default.conf to app/nodeapp.conf then recreated the VM

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -3,25 +3,25 @@
 # Update the sources list
 sudo apt-get update -y
 
-# upgrade any packages available
+# Upgrade any packages available
 sudo apt-get upgrade -y
 
-# install nginx
+# Install nginx
 sudo apt-get install nginx -y
 
-# install git
+# Install git
 sudo apt-get install git -y
 
-# install nodejs
+# Install nodejs
 sudo apt-get install python-software-properties
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
-# install pm2
+# Install pm2
 sudo npm install pm2 -g
 
 # Create and link file with the default file
-sudo ln -s /home/ubuntu/environment/default.conf /etc/nginx/conf.d/default.conf
+sudo ln -s /home/ubuntu/environment/nodeapp.conf /etc/nginx/conf.d/nodeapp.conf
 
 # Restart the server
 sudo nginx -s reload
